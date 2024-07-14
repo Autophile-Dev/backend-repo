eefac69021f4f2466dc84d68fab4c4c9801f218a
Autophile-Dev <waleed.zaheer55@gmail.com>
2024-07-15 00:21:18 +0800

    firebaseConfig changes

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,25 +1,10 @@
 import admin from 'firebase-admin';
-import dotenv from 'dotenv';
+const serviceAccountKey = require('./serviceAccountKey.json');
 
-dotenv.config();
-
-const serviceAccountKey: any = {
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    "universe_domain": "googleapis.com"
-};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccountKey),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    databaseURL: "https://console.firebase.google.com/u/0/project/ebuddy-test-5fbba/firestore/databases/-default-/data/~2F"
 });
 admin.firestore().listCollections()
     .then(() => {
